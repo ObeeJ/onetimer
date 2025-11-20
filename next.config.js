e9ee01bf4aab77cad85929f7d29b89ba4fd230eb
ObeeJ <Ajayioba2000@gmail.com>
2025-11-20 18:47:59 +0100

    standalone dependency

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   swcMinify: true,
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
